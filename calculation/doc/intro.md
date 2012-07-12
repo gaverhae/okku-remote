4f9030b181665a33eb561dba4bb3aca413f329b5
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-12 17:30:31 +0200

    Added calculation project (main "server")

---
+++ calculation/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to calculation
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ calculation/test/calculation/core_test.clj
@@ -0,0 +1,7 @@
+(ns calculation.core-test
+  (:use clojure.test
+        calculation.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
