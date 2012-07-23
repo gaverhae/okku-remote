8452235e58e070ebf48a627554b350c1998c1b95
Gary Verhaegen <gary.verhaegen@gmail.com>
2012-07-24 00:15:57 +0200

    Bumped version number to match Okku

--- calculation/project.clj
+++ calculation/project.clj
@@ -1,8 +1,8 @@
-(defproject calculation "0.1.2-SNAPSHOT"
+(defproject calculation "0.1.2"
   :description "Calculator part of the Okku remote tutorial"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main calculation.core
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure.gaverhae/okku "0.1.2-SNAPSHOT"]
-                 [common-actors "0.1.2-SNAPSHOT"]])
+                 [org.clojure.gaverhae/okku "0.1.2"]
+                 [common-actors "0.1.2"]])
